6848fb37fe1c2fb40c274d262ecf02e141ca5d80
JJRomeroCruz <juanjorcr98@gmail.com>
2026-04-02 14:37:55 +0200

    primera config

---
+++ config.py
@@ -0,0 +1,25 @@
+# Configuracion del email
+EMAIL_SENDER = "" # tu eamil
+EMAIL_PASSWORD = ""
+
+SMTP_SERVER = ""
+SMTP_PORT = 587
+
+# Usuarios y sus correos
+USERS = {
+    "Juano": "juanolas73@gmail.com"
+    "Julio": 
+    "Edu": 
+    "Verdín":
+    "Bal tosar":
+    "Carlos Honorio":
+    "Pablo":
+    "Luis Piso":
+    "Cuoc":
+    "Beni":
+    "Currito":
+    "Concha":
+}
+
+MIN_NUMBER = 0
+MAX_NUMBER = 7
